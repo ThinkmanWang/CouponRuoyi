4f1d46cbb128fb1292e38d75abee5863ab930223
Thinkman Wang <wangxf1985@gmail.com>
2019-11-12 17:26:22 +0800

    update

---
+++ ruoyi-ui/src/views/test/hello.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrapper">
+      FXXXXK
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    export default {
+        name: "hello"
+        , data() {
+            return {}
+        }
+        , created() {
+        }
+        , mounted() {
+        }
+        , beforeDestroy() {
+        }
+        , watch: {}
+        , computed: {}
+        , methods: {}
+    }
+</script>
+
