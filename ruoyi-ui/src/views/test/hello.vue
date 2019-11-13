664eaa82d0417d2c31e2b5a9622147b36425c8b5
Thinkman Wang <wangxf1985@gmail.com>
2019-11-13 15:39:40 +0800

    update

--- ruoyi-ui/src/views/test/hello.vue
+++ ruoyi-ui/src/views/test/hello.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="wrapper">
       FXXXXK
+      <el-button
+        type="primary"
+        icon="el-icon-search"
+        size="mini"
+        v-hasPermi="['business:coupon:brand:list']"
+        @click="handleQuery"
+      >查询</el-button>
+
+      <el-button
+        type="primary"
+        icon="el-icon-edit"
+        size="mini"
+        v-hasPermi="['business:coupon:brand:add']"
+        @click="handleAdd"
+      >添加</el-button>
     </div>
 </template>
 <style lang="scss" scoped>
@@ -21,7 +36,14 @@
         }
         , watch: {}
         , computed: {}
-        , methods: {}
+        , methods: {
+            handleQuery() {
+
+            }
+            , handleAdd() {
+
+            }
+        }
     }
 </script>
 
